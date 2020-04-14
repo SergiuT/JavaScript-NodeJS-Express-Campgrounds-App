d94e9cda4738bc39e2e1679b50bf1d6002c76127
TONT SERGIU <49995212+SergiuT@users.noreply.github.com>
2020-04-14 22:36:43 +0300

    Refactoring

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-var express=require("express"),
-    app=express(),
-    bodyParser=require("body-parser"),
-    mongoose=require("mongoose"),
-    passport=require("passport"),
-    flash=require("connect-flash"),
-    LocalStrategy=require("passport-local"),
-    Comment=require("./models/comment"),
-    Campground=require("./models/campground"),
-    User=require("./models/user"),
-    methodOverride=require("method-override"),
-    seedDB=require("./seeds");
-    
-    
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    flash = require("connect-flash"),
+    LocalStrategy = require("passport-local"),
+    Comment = require("./models/comment"),
+    Campground = require("./models/campground"),
+    User = require("./models/user"),
+    methodOverride = require("method-override"),
+    seedDB = require("./seeds");
+
+
 
 //Requiring routes    
-var commentRoutes=require("./routes/comments"),
-    campgroundRoutes=require("./routes/campgrounds"),
-    authRoutes=require("./routes/index"),
-    indexRoutes      = require("./routes/index"),
-    reviewRoutes     = require("./routes/review");
+var commentRoutes = require("./routes/comments"),
+    campgroundRoutes = require("./routes/campgrounds"),
+    authRoutes = require("./routes/index"),
+    indexRoutes = require("./routes/index"),
+    reviewRoutes = require("./routes/review");
 
-    
+
 //seedDB();
-mongoose.connect("mongodb://localhost:27017/yelp_camp_v8", {useNewUrlParser: true});
-app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine","ejs");
-app.use(express.static(__dirname+"/public"));
+mongoose.connect("mongodb+srv://sergiuT:yelpcamp1@yelpcamp-sdlrf.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true });
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
 app.locals.moment = require('moment');
@@ -43,19 +43,19 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req, res, next){
-    res.locals.currentUser= req.user;
-    res.locals.error=req.flash("error");
-    res.locals.success=req.flash("success");
+app.use(function (req, res, next) {
+    res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
     next();
 });
 
 app.use(authRoutes);
 app.use("/", indexRoutes);
-app.use("/campgrounds/:id/comments",commentRoutes);
-app.use("/campgrounds",campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/reviews", reviewRoutes);
 
-app.listen(process.env.PORT, process.env.IP,function(){
-   console.log("YelpCamp server has started"); 
+app.listen(3000, process.env.IP, function () {
+    console.log("YelpCamp server has started");
 });
